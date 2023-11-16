5c8dee45ecee3dc9d6533faff1e3ae3dabec8ce2
macaa <whaud1597@naver.com>
2023-11-16 20:02:30 +0900

    json null 지움

--- src/main/java/music/musicsite/config/AppConfig.java
+++ src/main/java/music/musicsite/config/AppConfig.java
@@ -8,11 +8,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    //Jackson에서 즉 스프링에서 값을 반환할 때 객체에서 null값이 반환되는 경우가 있는데 그때 null값은 보내지 않기 위해 사용
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        return objectMapper;
-    }
 }
